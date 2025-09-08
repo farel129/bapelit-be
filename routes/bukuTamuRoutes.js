0c1c0191f1b001576069e22499b8da205c88d14a
farelkdhfi <farelkadhafi34@gmail.com>
2025-09-08 20:56:54 +0700

    repair architecture

--- routes/bukuTamuRoutes.js
+++ routes/bukuTamuRoutes.js
@@ -26,7 +26,7 @@ router.get('/', authenticateToken, requireAdmin, getBukuTamu)
 router.delete('/:id', authenticateToken, requireAdmin, deleteBukuTamu)
 router.get('/:id/tamu', authenticateToken, requireAdmin, getListTamu)
 router.patch('/:id/status', authenticateToken, requireAdmin, updateStatusBukuTamu)
-router.delete('/:foto_id', authenticateToken, requireAdmin, deleteFotoTamu)
+router.delete('/foto/:foto_id', authenticateToken, requireAdmin, deleteFotoTamu)
 
 // ==========================public===========================//
 router.post('/:qr_token/check-device', checkDevice)
